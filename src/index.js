25931d660f66e4c80e468f02f0d3c92fc533425d
Erin Bassity <erin.bassity@cognizant.com>
2018-07-02 13:58:59 -0600

    Update users list in sidebar
    * Change variable type

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-
-import chat from './reducers/index'
+import { addUser } from './actions'
+import chat from './reducers'
 
 const store = createStore(chat)
+
+store.dispatch(addUser('Me'))
 
 ReactDOM.render(
   <Provider store={store}>
