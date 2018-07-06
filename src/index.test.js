0a10ab9745377819ee6b65b4c221e0739d13206e
Erin Bassity <erin.bassity@cognizant.com>
2018-07-06 16:14:07 -0600

    Add second message test

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,8 @@
 import { addMessage, addUser } from './actions'
 import * as types from './constants/ActionTypes'
 
-describe('add a message', () => {
-  it('should create an action with an id of 0 to add a message', () => {
+describe('can add a message', () => {
+  it('can create an action to add a message with an id of 0', () => {
     const message = 'This is a message'
     const author = 'Renalda'
     const action = {
@@ -10,6 +10,20 @@ describe('add a message', () => {
       message,
       author: 'Renalda',
       id: 0
+    }
+    expect(addMessage(message, author)).toEqual(action)
+  })
+})
+
+describe('can add a second message', () => {
+  it('can create an action to add a message with an id of 1', () => {
+    const message = 'This is a second message!'
+    const author = 'Sally OMalley'
+    const action = {
+      type: types.ADD_MESSAGE,
+      message,
+      author: 'Sally OMalley',
+      id: 1
     }
     expect(addMessage(message, author)).toEqual(action)
   })
