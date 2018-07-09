f6b76233168f1a826864bfac1a364fa26461c73b
Erin Bassity <erin.bassity@cognizant.com>
2018-07-09 15:30:43 -0600

    Add redux-saga

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { addUser } from './actions'
-import chat from './reducers'
+import reducers from './reducers'
+import handleNewMessage from './sagas'
+import setupSocket from './sockets'
+import username from './utils/name'
 
-const store = createStore(chat)
+const store = createStore(
+  reducers,)
 
 store.dispatch(addUser('Erin'))
 
